951dce5b303265f837e05eb342fe4a85f54d7899
Zxilly <zxilly@outlook.com>
2022-05-08 11:39:36 +0800

    fix: fix conflict resolve error
    Signed-off-by: Zxilly <zxilly@outlook.com>

--- app/src/main/kotlin/top/learningman/enter/MainActivity.kt
+++ app/src/main/kotlin/top/learningman/enter/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
