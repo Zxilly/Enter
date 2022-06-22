266a6b7d0088a132fd194eb32e5d09be3f997aa0
Zxilly <zhouxinyu1001@gmail.com>
2022-06-22 11:19:09 +0800

    feat: add MainApplication
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

---
+++ app/src/main/kotlin/top/learningman/enter/MainApplication.kt
@@ -0,0 +1,17 @@
+package top.learningman.enter
+
+import android.app.Application
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.distribute.Distribute
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppCenter.start(
+            this, "bdb6695f-b9af-49dd-ae56-2a2bdd4232c8",
+            Analytics::class.java, Crashes::class.java, Distribute::class.java
+        )
+    }
+}
